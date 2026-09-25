7fc264ed3af384934a2b0ba594f57c587463809b
Jorge Perez <jorgeimon100@gmail.com>
2026-09-25 13:05:03 +0200

    trabajo

--- scripts/Obstacle.cs
+++ scripts/Obstacle.cs
@@ -1,23 +1,45 @@
+using System.Drawing;
+using Unity.Collections.Tests.CoreCLR.TestJobs;
 using UnityEngine;
 
 public class ObstaculerController : MonoBehaviour
 {
-
+[SerializeField]
     private float minSize = 0.3f;
 
+    [SerializeField]
      private float maxSize = 2f;
+
+     [SerializeField]
+     private float minForce = 10f;
+
+     [SerializeField]
+     private float maxForce = 20f;
+
+     [SerializeField]
+     private float minTorque = 10f;
+     [SerializeField]
+        private float maxTorque = 20f;
+    
+     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-        transform.localScale = new Vector3(Random.Range(minSize, maxSize), 3, 1);
+        float size = 
+        Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(size,size , 1);
          Rigidbody2D rb = GetComponent<Rigidbody2D>();
          Transform tf = GetComponent<Transform>();
          Debug.Log(transform == tf);
-         rb.AddForce(Vector2.up * 34);
+         Vector2 randomDirection = Random.insideUnitCircle;
+         rb.AddForce(randomDirection * 34);
+         float Torque = Random.Range(minTorque, maxTorque);
+         rb.AddTorque(Torque);
 
 
-        
+                float force = Random.Range(minForce, maxForce);
+                rb.AddForce(randomDirection * force);
+
     }
 
 
