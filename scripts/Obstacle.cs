3840117a0f0e3745f378e5627a87ba0c23c33c43
Jorge Perez <jorgeimon100@gmail.com>
2026-09-25 12:23:16 +0200

    juego

--- scripts/Obstacle.cs
+++ scripts/Obstacle.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 public class ObstaculerController : MonoBehaviour
 {
+
+    private float minSize = 0.3f;
+
+     private float maxSize = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        transform.localScale = new Vector3(Random.Range(minSize, maxSize), 3, 1);
+         Rigidbody2D rb = GetComponent<Rigidbody2D>();
+         Transform tf = GetComponent<Transform>();
+         Debug.Log(transform == tf);
+         rb.AddForce(Vector2.up * 34);
+
+
+        
     }
+
+
 
     // Update is called once per frame
     void Update()
